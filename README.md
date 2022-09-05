6eebff7f33222f6e2b5c7ebfb652865655b00379
Eduardo Developer <eduardotecnologo@hotmail.com>
2022-09-05 19:44:40 -0300

    Create Project Spring React

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sds-dsmeta
 Estudos React, Spring
+- Create Project vite
+yarn create vite frontend --template react-ts
---
+++ backend/src/test/java/com/edudeveloper/appmetas/AppmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudeveloper.appmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
